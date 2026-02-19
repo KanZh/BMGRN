237a472bc4f68797178014b209bcb1159666c2fe
KanZh <139417429+KanZh@users.noreply.github.com>
2026-02-19 14:42:07 +0800

    Add files via upload

--- Ecoli_heat_FGRN/corrresnet_pred.py
+++ Ecoli_heat_FGRN/corrresnet_pred.py
@@ -1,14 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
 import numpy as np
 from sklearn.utils import class_weight
 import torch.nn.functional as F
 from torch.utils.data import TensorDataset, DataLoader
-from torch.optim.lr_scheduler import ReduceLROnPlateau
 from KANConv import KANConv1DLayer as KANConv
-from mamba2 import NdMamba2_1d
+from nd_mamba2_fix import NdMamba2_1d
 import time
 
 def count_parameters(model):
@@ -30,10 +28,7 @@ class Classifier(nn.Module):
         self.patience = 5
         self.output_directory = output_directory
 
-        self.kan1 = KAN([24, 24])
-        self.kan2 = KAN([24, 24])
-
-        self.mamba = NdMamba2_1d(2, 128, 128, True)
+        self.mamba = NdMamba2_1d(2, 128, 128)
 
 
         self.global_avg_pool = nn.AdaptiveAvgPool1d(1)
@@ -41,10 +36,8 @@ class Classifier(nn.Module):
         self.dropout = nn.Dropout(0.3)
         self.fc2 = nn.Linear(256, 128)
 
-        # Linear layer to match Transformer input dimension
         self.fc_transformer_input = nn.Linear(640, 768)
 
-        # self.conv = nn.Conv1d(640, 16, 15, padding=7)
         self.conv = KANConv(640, 16, 15, padding=10)
 
         self.bn = nn.BatchNorm1d(16, momentum=0.8)
@@ -56,7 +49,6 @@ class Classifier(nn.Module):
 
     def forward(self, x_train1, x_train2, net_emd_tf_s, net_emd_tf_t, net_emd_target_s, net_emd_target_t):
 
-        # Ensure inputs are on the correct device
         x_train1, x_train2 = x_train1.to(self.device), x_train2.to(self.device)
         net_emd_tf_s, net_emd_tf_t = net_emd_tf_s.to(self.device), net_emd_tf_t.to(self.device)
         net_emd_target_s, net_emd_target_t = net_emd_target_s.to(self.device), net_emd_target_t.to(self.device)
@@ -65,27 +57,42 @@ class Classifier(nn.Module):
         f2 = x_train2.squeeze(2)
         f1 = f1.unsqueeze(1)
         f2 = f2.unsqueeze(1)
+        # f1 = self.embedding1(x1)
+        # f2 = self.embedding2(x2)
+        # print(f'f1 size{f1.shape}')
+        mask_f1 = torch.ones(f1.size()[:-1], dtype=torch.bool, device=f1.device)
+        mask_f2 = torch.ones(f2.size()[:-1], dtype=torch.bool, device=f2.device)
+        # print(f'mask size{mask_f1.shape}')
 
-
-        prot_features = self.kan1(f1)
-        drug_features = self.kan2(f2)
-        combined_features = torch.cat([prot_features, drug_features], dim=1)
+        combined_features = torch.cat([f1, f2], dim=1)
         combined_features = self.mamba(combined_features)
 
+        if torch.isnan(combined_features).any():
+            print("NaN detected after Mamba combination")
+            import pdb;
+            pdb.set_trace()
 
         combined_features = F.relu(self.fc1(combined_features))
         combined_features = self.dropout(combined_features)
         combined_features = F.relu(self.fc2(combined_features))
+        # out = self.fc_pred2(combined_features)
+        # print(f'combined feature size{combined_features.shape}')
         input_layer_net_tf_s_ = net_emd_tf_s.squeeze(1)
         input_layer_net_tf_t_ = net_emd_tf_t.squeeze(1)
         input_layer_net_target_s_ = net_emd_target_s.squeeze(1)
         input_layer_net_target_t_ = net_emd_target_t.squeeze(1)
+        # print(f'combined_features shape: {combined_features.shape}')
+        # print(f'input_layer_net_tf_s_ shape: {input_layer_net_tf_s_.shape}')
+        # print(f'input_layer_net_tf_t_ shape: {input_layer_net_tf_t_.shape}')
+        # print(f'input_layer_net_target_s_ shape: {input_layer_net_target_s_.shape}')
+        # print(f'input_layer_net_target_t_ shape: {input_layer_net_target_t_.shape}')
         combined_features = combined_features.mean(dim=1)
 
         # Concatenate features
         all_features = torch.cat(
             [combined_features, input_layer_net_tf_s_, input_layer_net_tf_t_, input_layer_net_target_s_,
              input_layer_net_target_t_], dim=1)
+        # print(f'all_features shape: {all_features.shape}')
 
         # Reshape
         all_features = all_features.unsqueeze(1).permute(0, 2, 1)
@@ -95,6 +102,8 @@ class Classifier(nn.Module):
         x = self.bn(x)
         x = self.pool(x)
         x = F.relu(x)
+
+        # Global Average Pooling
         x = self.global_avg_pool(x)
         x = x.view(x.size(0), -1)  # Flatten the output for dense layer
 
@@ -102,6 +111,7 @@ class Classifier(nn.Module):
         x = F.relu(self.fc_pred2(x))
         x = self.dropout(x)
         x = self.fc_final(x)
+        # print(f'output shape: {x.shape}')
         return x
 
     def fit_5CV(self, x_train_1, x_train_2, net_emd_tf_s_train, net_emd_tf_t_train, net_emd_target_s_train,
@@ -112,7 +122,8 @@ class Classifier(nn.Module):
         start_time = time.time()
 
         batch_size = 128
-        nb_epochs = 150
+        # nb_epochs = 150
+        nb_epochs = 80
 
         y_train_num = []
         for i in range(y_train.shape[0]):
@@ -129,8 +140,6 @@ class Classifier(nn.Module):
             else:
                 print('error y-train')
         y_train_num = np.array(y_train_num)
-
-
         class_weights = class_weight.compute_class_weight(
             class_weight='balanced',
             classes=np.unique(y_train_num),
@@ -144,7 +153,16 @@ class Classifier(nn.Module):
         print(class_weights)
         print('------------------------------------------------------------------------------')
         mini_batch_size = int(min(x_train_1.shape[0] / 10, batch_size))
+        # Print dimensions before training loop
+        # print(f'x_train_1 shape: {x_train_1.shape}')
+        # print(f'x_train_2 shape: {x_train_2.shape}')
+        # print(f'net_emd_tf_s_train shape: {net_emd_tf_s_train.shape}')
+        # print(f'net_emd_tf_t_train shape: {net_emd_tf_t_train.shape}')
+        # print(f'net_emd_target_s_train shape: {net_emd_target_s_train.shape}')
+        # print(f'net_emd_target_t_train shape: {net_emd_target_t_train.shape}')
+        # print(f'y_train shape: {y_train.shape}')
 
+        # ======================================== pytorch-train =============================================
 
         train_dataset = TensorDataset(x_train_1, x_train_2, net_emd_tf_s_train, net_emd_tf_t_train,
                                       net_emd_target_s_train, net_emd_target_t_train, y_train)
@@ -157,10 +175,15 @@ class Classifier(nn.Module):
         optimizer = optim.AdamW(self.parameters(),
                               lr=1e-3,
                               weight_decay=0.01)
+        # scheduler = ReduceLROnPlateau(optimizer, 'max', factor=0.001, patience=int(self.patience / 2), verbose=True)
         scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
         criterion = nn.CrossEntropyLoss(weight=class_weights)
 
+        patience = 0
         best_val_acc = 0
+        last_acc = 0
+
+        max_grad_norm = 1.0
 
         for epoch in range(nb_epochs):
 
@@ -182,13 +205,14 @@ class Classifier(nn.Module):
                 loss = criterion(outputs, labels)
                 loss.backward()
 
-                torch.nn.utils.clip_grad_norm_(self.parameters(), max_grad_norm)  # 添加这行
+                torch.nn.utils.clip_grad_norm_(self.parameters(), max_grad_norm)
 
                 optimizer.step()
                 total_loss += loss.item()
 
             current_lr = optimizer.param_groups[0]['lr']
             print(f'Epoch: {epoch}, Batch: {i}, Current LR: {current_lr}')
+
 
             self.eval()
             total = 0
@@ -208,11 +232,21 @@ class Classifier(nn.Module):
                     correct += (predicted == labels).sum().item()
 
             val_acc = correct / total
+            # scheduler.step(val_acc)
             scheduler.step()
 
             if val_acc > best_val_acc:
                 best_val_acc = val_acc
                 torch.save(self.state_dict(), self.output_directory + 'best_model.pth')
+            #     patience = 0
+            # elif val_acc > last_acc:
+            #     patience -= 1
+            # else:
+            #     patience += 1
+            #     if patience >= self.patience:
+            #         print("Early stopping due to no improvement")
+            #         break
+            # last_acc = val_acc
 
             epoch_end_time = time.time()
             epoch_duration = epoch_end_time - epoch_start_time
